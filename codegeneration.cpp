3a209e0b2fd1c7befc8dae6228442341e6d17275
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-26 12:17:06 -0700

    fixed warnings about paths not returning anything in a non-void function

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -140,10 +140,8 @@ void CodeGenerator::visitPrintNode(PrintNode* node) {
     std::cout << "      push %eax" << "               # push the result of the return statement." << std::endl;
     std::cout << "      push $printstr" << "          # load format to be used for printing." << std::endl;
     std::cout << "      call printf" << "             # print value in the return expression." << std::endl;
-    std::cout << "      call printf" << "             # print value in the return expression." << std::endl;
-    
-    
-    //std::cout << "      call printf" << "             # print value in the return expression." << std::endl;
+    std::cout << "      pop %eax" << "                # load format to be used for printing." << std::endl;
+    std::cout << "      pop %eax" << "                # push the result of the return statement." << std::endl;
 }
 
 void CodeGenerator::visitDoWhileNode(DoWhileNode* node) {

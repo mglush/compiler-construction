949d5c7464e19bbeb6b5fdc27ed1ae001a867c28
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-26 14:00:17 -0700

    fixed warnings about paths not returning anything in a non-void function

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -90,7 +90,7 @@ void CodeGenerator::visitMethodBodyNode(MethodBodyNode* node) {
 
     // function callee epilogue.
     if (COMMENTS_ON) std::cout << getOffset(TAB_COUNTER) << "# Starting callee function epilogue." << std::endl;
-    std::cout << getOffset(TAB_COUNTER) << "pop %eax" << "              # save the return value in %eax as per __cdecl convention." << std::endl;
+    std::cout << getOffset(TAB_COUNTER) << "pop %eax" << "                          # save the return value in %eax as per __cdecl convention." << std::endl;
 
     std::cout << getOffset(TAB_COUNTER) << "pop %edi" << "                          # callee responsible for preserving contents of this register." << std::endl;
     std::cout << getOffset(TAB_COUNTER) << "pop %esi" << "                          # callee responsible for preserving contents of this register." << std::endl;
@@ -361,8 +361,8 @@ void CodeGenerator::visitVariableNode(VariableNode* node) {
     TAB_COUNTER++;
     if (COMMENTS_ON) std::cout << getOffset(TAB_COUNTER) << "# Visiting Variable." << std::endl;
     std::cout << getOffset(TAB_COUNTER) << "mov " << findVariableOffset(this, node->identifier->name) << "(%ebp)" << ", %eax";
-    std::cout << getOffset(TAB_COUNTER) << "                 # load the variable value from the right place in memory." << std::endl;
-    std::cout << getOffset(TAB_COUNTER) << "push %eax" << "                          # put it on top of the stack." << std::endl;
+    std::cout << getOffset(TAB_COUNTER) << "               # load the variable value from the right place in memory." << std::endl;
+    std::cout << getOffset(TAB_COUNTER) << "push %eax" << "                        # put it on top of the stack." << std::endl;
     TAB_COUNTER--;
 }
 

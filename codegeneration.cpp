981b37f7feaf9520b12cd40abd50b7e07942c6fd
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-26 10:33:17 -0700

    fixed warnings about paths not returning anything in a non-void function

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -75,11 +75,11 @@ void CodeGenerator::visitMethodBodyNode(MethodBodyNode* node) {
 
     node->visit_children(this);
 
-    std::cout << "      # printing after da return statement!" << std::endl;
-    // for testing purposes, we will print out the value returned by a method.
-    std::cout << "      # print returned value for testing purposes only. next 3 lines of code should be removed when submitting final version." << std::endl;
-    std::cout << "      push $printstr" << "          # load format to be used for printing." << std::endl;
-    std::cout << "      call printf" << "             # print value in the return expression." << std::endl;
+    // std::cout << "      # printing after da return statement!" << std::endl;
+    // // for testing purposes, we will print out the value returned by a method.
+    // std::cout << "      # print returned value for testing purposes only. next 3 lines of code should be removed when submitting final version." << std::endl;
+    // std::cout << "      push $printstr" << "          # load format to be used for printing." << std::endl;
+    // std::cout << "      call printf" << "             # print value in the return expression." << std::endl;
 
     // function callee epilogue.
     std::cout << "# Starting callee function epilogue." << std::endl;
@@ -138,6 +138,7 @@ void CodeGenerator::visitPrintNode(PrintNode* node) {
     std::cout << "      # print returned value for testing purposes only. next 3 lines of code should be removed when submitting final version." << std::endl;
     std::cout << "      push $printstr" << "          # load format to be used for printing." << std::endl;
     std::cout << "      call printf" << "             # print value in the return expression." << std::endl;
+    std::cout << "      add $4, %esp" << "            # move stack pointer back to where it belongs" << std::endl;
 }
 
 void CodeGenerator::visitDoWhileNode(DoWhileNode* node) {

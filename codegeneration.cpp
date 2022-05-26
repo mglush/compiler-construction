ce98c5ce3bd67cf80a5fc58e3f99e2ae5b61e033
Mykhailo Glushchenko <glush@169-231-100-13.wireless.ucsb.edu>
2022-05-26 00:46:30 -0700

    fixed warnings about paths not returning anything in a non-void function

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -19,7 +19,6 @@ int findVariableOffset(CodeGenerator* visitor, std::string name) {
 }
 
 void CodeGenerator::visitProgramNode(ProgramNode* node) {
-    std::cout << "\n------------------------------------\n" << "ASSEMBLY CODE:" << "\n------------------------------------\n";
     std::cout << ".globl Main_main" << "          # tell the linker Main_main is a callable function." << std::endl;
     node->visit_children(this);
 }

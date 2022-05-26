84d64ebf503ba8f6827248e49d65c765edd13982
Mykhailo Glushchenko <glush@169-231-100-13.wireless.ucsb.edu>
2022-05-25 22:14:53 -0700

    fixed warnings about paths not returning anything in a non-void function

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -227,6 +227,8 @@ void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
     std::cout << "          push $ecx" << "       # caller responsible for preserving contents of this register." << std::endl;
     std::cout << "          push $edx" << "       # caller responsible for preserving contents of this register." << std::endl;
     
+    std::cout << "      # making a method call here." << std::endl;
+
     node->visit_children(this);
 
     std::cout << "          pop $edx" << "        # caller responsible for preserving contents of this register." << std::endl;

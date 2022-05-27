f397e7d27148621e9dd89b9ead56f02d8564956e
Mykhailo Glushchenko <glush@169-231-100-35.wireless.ucsb.edu>
2022-05-26 20:36:13 -0700

    fixed warnings about paths not returning anything in a non-void function

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -226,10 +226,10 @@ void CodeGenerator::visitDivideNode(DivideNode* node) {
     node->visit_children(this);
 
     if (COMMENTS_ON) std::cout << getOffset(TAB_COUNTER) << "# Processing DivideNode." << std::endl;
-    std::cout << getOffset(TAB_COUNTER) << "popl %edx" << std::endl;
-    std::cout << getOffset(TAB_COUNTER) << "popl %eax" << std::endl;
-    std::cout << getOffset(TAB_COUNTER) << "idivl %edx, %eax" << std::endl;
-    std::cout << getOffset(TAB_COUNTER) << "pushl %eax" << std::endl;
+    std::cout << getOffset(TAB_COUNTER) << "pop %ecx" << std::endl;
+    std::cout << getOffset(TAB_COUNTER) << "pop %eax" << std::endl;
+    std::cout << getOffset(TAB_COUNTER) << "idiv %ecx, %eax" << std::endl;
+    std::cout << getOffset(TAB_COUNTER) << "push %eax" << std::endl;
     TAB_COUNTER--;
 }
 

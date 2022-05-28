a399a45342a1c565e984c0ec0114c314537be242
Mykhailo Glushchenko <glush@169-231-109-66.wireless.ucsb.edu>
2022-05-27 23:29:07 -0700

    starting working on flow control here. first commit - ifelsenode

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -384,9 +384,7 @@ void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
     } else {
         // push object self pointer to the stack if caller is any function other than main.
         std::cout << getIndent(TAB_COUNTER) << "push %ebp" << "                        # push the current object self pointer onto the stack." << std::endl;
-        
-        // push return address onto the stack.
-        // std::cout << getIndent(TAB_COUNTER) << "push -8(%esp)" << "             `          # push the return address onto the stack" << std::endl;
+        // perform the method call.
         std::cout << getIndent(TAB_COUNTER) << "call " << this->currentClassName << "_" << node->identifier_1->name << std::endl;
     }
     

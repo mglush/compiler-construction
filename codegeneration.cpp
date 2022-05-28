94322a00059ee0bca9903181062c3e7c4c7ca36f
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-28 12:08:07 -0700

    working on implementing objects into the code generation process

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -416,7 +416,7 @@ void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
     if (node->identifier_2) {
         // after finding the variable, gets its class object name, and call the appropriate method baby.
         std::cout << getIndent(TAB_COUNTER) << "push %ebp" << "                        # push the current object self pointer onto the stack." << std::endl;
-        std::cout << getIndent(TAB_COUNTER) << "call " << findVariableObjectName(this, node->identifier_1->name) << "_" << node->identifier_1->name;
+        std::cout << getIndent(TAB_COUNTER) << "call " << findVariableObjectName(this, node->identifier_1->name) << "_" << node->identifier_2->name;
         std::cout << "                     # perform the appropriate method call." << std::endl;
     } else {
         std::cout << getIndent(TAB_COUNTER) << "push %ebp" << "                        # push the current object self pointer onto the stack." << std::endl;

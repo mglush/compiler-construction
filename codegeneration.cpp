bef5d280818f8a7cd0c81f796727d4e3ada44fcb
Mykhailo Glushchenko <glush@169-231-109-66.wireless.ucsb.edu>
2022-05-28 17:09:14 -0700

    working on implementing objects into the code generation process

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -27,11 +27,10 @@ int findVariableOffset(CodeGenerator* visitor, std::string class_name, std::stri
        return visitor->currentMethodInfo.variables->at(name).offset;
     }
     else {
+        std::cout << class_name << std::endl;
         if (visitor->classTable->at(class_name).members->count(name))
             return visitor->classTable->at(class_name).members->at(name).offset;
-        
         class_name = visitor->classTable->at(class_name).superClassName;
-        std::cout << class_name << std::endl;
         while (visitor->classTable->at(class_name).members->count(name) == 0) {
             result += visitor->classTable->at(class_name).membersSize;
             class_name = visitor->classTable->at(class_name).superClassName;

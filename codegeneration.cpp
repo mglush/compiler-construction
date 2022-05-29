9ce8accecad1ec8fb62847a35534d05445d5f0ea
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-29 01:23:36 -0700

    parameternode edit

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -69,7 +69,7 @@ void modifySymbolTable(CodeGenerator* visitor) {
     for (std::map<std::string, ClassInfo>::iterator it = visitor->classTable->begin(); it != visitor->classTable->end(); it++) {
         superclass = it->second.superClassName;
         while (superclass.length()) {
-            std::map<std::string, VariableInfo> yomp = visitor->classTable->at(superclass).members;
+            std::map<std::string, VariableInfo>* yomp = visitor->classTable->at(superclass).members;
             it->second.members->insert(yomp);
             superclass = visitor->classTable->at(superclass).superClassName;
         }

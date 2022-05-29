41bcef7585c615bcd722f05198512e15b87d786c
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-29 02:52:27 -0700

    parameternode edit

--- typecheck.cpp
+++ typecheck.cpp
@@ -267,14 +267,18 @@ void methodArgumentTypeMismatch(TypeCheck* visitor, std::list<ExpressionNode*>* 
 // into the child class variable tables.
 void modifySymbolTable(TypeCheck* visitor) {
     std::string superclass;
+    int counter;
     for (std::map<std::string, ClassInfo>::iterator it = visitor->classTable->begin(); it != visitor->classTable->end(); it++) {
         superclass = it->second.superClassName;
         while (superclass.length()) {
           // go throough every member variable, and increase its offset.
-          for (std::map<std::string, VariableInfo>::reverse_iterator iter = visitor->classTable->at(superclass).members->rbegin(); iter != visitor->classTable->at(superclass).members->rend(); iter++) {
+          counter = 0;
+          for (std::map<std::string, VariableInfo>::reverse_iterator iter = visitor->classTable->at(superclass).members->rbegin(); iter != visitor->classTable->at(superclass).members->rend(); iter++) {  
             VariableInfo newVariableInfo = {{iter->second.type.baseType, iter->second.type.objectClassName}, iter->second.offset + it->second.membersSize, iter->second.size};
             it->second.members->insert(std::make_pair(iter->first, newVariableInfo));
+            counter++
           }
+          it->second.membersSize += 4 * counter;
           superclass = visitor->classTable->at(superclass).superClassName;
         }
     }

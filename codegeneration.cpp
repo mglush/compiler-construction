df4b67df962fc5445d048fb995c64fd7a8f354de
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-29 21:00:23 -0700

    working on inheritance

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -433,6 +433,7 @@ void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
         while (!(this->classTable->at(object_name).methods->count(node->identifier_2->name)))
             object_name = this->classTable->at(object_name).superClassName;
 
+        std::cout << "\n\n\n" << this->currentClassName << "\n\n\n";
         std::cout << findVariableOffset(this, this->currentClassName, node->identifier_1->name) << "(%ebp), %ebx";
         std::cout << getIndent(TAB_COUNTER) << "              # get the object self pointer from the right place in memory, put it into %ebx." << std::endl << std::endl;
         std::cout << getIndent(TAB_COUNTER) << "push %ebx" << "                        # push the receiver object self pointer." << std::endl;

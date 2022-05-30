59d8e33cbf26f6b664a7d6ae4145a5daa7046436
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-29 19:06:24 -0700

    working on inheritance

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -25,14 +25,8 @@ int findVariableOffset(CodeGenerator* visitor, std::string class_name, std::stri
 
 // helper function to find the proper offset of a given class's member.
 int findMemberOffset(CodeGenerator* visitor, std::string class_name, std::string name) {
-    int result;
     if (visitor->classTable->at(class_name).members->count(name))
-        result = visitor->classTable->at(class_name).members->at(name).offset;
-    class_name = visitor->classTable->at(class_name).superClassName;
-    while (class_name.length()) {
-        result += visitor->classTable->at(class_name).membersSize;
-        class_name = visitor->classTable->at(class_name).superClassName;
-    }
+        return visitor->classTable->at(class_name).members->at(name).offset;
 }
 
 // helper function to find the classObjectName of a variable with the given name.

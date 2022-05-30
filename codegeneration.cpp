c32ab200477c818d0fb0c59451c8789bfd2ae19b
Mykhailo Glushchenko <glush@Mykhailos-MacBook-Pro.local>
2022-05-29 18:05:01 -0700

    working on inheritance

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -517,7 +517,7 @@ void CodeGenerator::visitMemberAccessNode(MemberAccessNode* node) {
         std::cout << getIndent(TAB_COUNTER) << "              # get the object self pointer from the right place in memory, put it into %ebx." << std::endl;
     }
     std::cout << getIndent(TAB_COUNTER) << "push " << findMemberOffset(this, findVariableObjectName(this, this->currentClassName, node->identifier_1->name), node->identifier_2->name) << "(%ebx)";
-    std::cout << getIndent(TAB_COUNTER) << "              # store value of right-hand side expression at the right offset from the object self pointer." << std::endl << std::endl;
+    std::cout << getIndent(TAB_COUNTER) << "              # IMPORTANT TO CHECK!!!." << std::endl << std::endl;
 }
 
 void CodeGenerator::visitNewNode(NewNode* node) {
